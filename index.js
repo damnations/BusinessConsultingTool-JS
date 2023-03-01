74cf0a1a1726053f127bd6943b4403051a9b9af9
Habeebullah Lawal <81315552+damnations@users.noreply.github.com>
2023-03-01 12:25:12 +0200

    create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+import mongoose from 'mongoose'
+import Customer from './models/Customer';
+
+mongoose.connect("mongodb+srv://<username>:<password>@cluster0.eyhty.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
